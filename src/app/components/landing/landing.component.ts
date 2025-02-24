bb2f0511249a8db195bd6d357f4c4c5a9ccfa681
Alex Bellosta <abellostabuiza@gmail.com>
2025-02-24 19:24:55 +0100

    dasd

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
+import { BestsellersComponent } from './bestsellers/bestsellers.component';
+import { NewProductsComponent } from './new-products/new-products.component';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [HeroComponent, BestsellersComponent, NewProductsComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
-
+  
 }
