7b8879bb02be82b22550342999bae7aa6c44f4c0
Alex Bellosta <abellostabuiza@gmail.com>
2025-02-26 19:56:20 +0100

    bhj

--- src/app/components/back-office/back-office.component.ts
+++ src/app/components/back-office/back-office.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+interface Product {
+  reference: string;
+  name: string;
+  price: number;
+  description: string;
+  category: string;
+  sale: boolean;
+  image: string;
+}
 
 @Component({
   selector: 'app-back-office',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './back-office.component.html',
-  styleUrl: './back-office.component.css'
+  styleUrls: ['./back-office.component.css']
 })
 export class BackOfficeComponent {
+  reference: string = "";
 
+  productForm = new FormGroup({
+    reference: new FormControl(''),
+    name: new FormControl(''),
+    price: new FormControl(0),
+    description: new FormControl(''),
+    category: new FormControl(''),
+    sale: new FormControl(false),
+    image: new FormControl(''),
+  });
+
+  onSubmit() {
+    if (this.productForm.valid) {
+      console.log('Datos del formulario:', this.productForm.value);
+    } else {
+      console.log('El formulario contiene errores.');
+    }
+  }
 }
